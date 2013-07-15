cdc76d4bf0930b1970e33e4d47d2adff5f5ebbfc
Marcus Hobbs <dhobbs81@gmail.com>
2013-07-15 17:26:03 -0400

    This is a test of my local repository.

---
+++ HelloWorldGAE/src/com/hellowordgae/mdh/HelloWorldGAEServlet.java
@@ -0,0 +1,13 @@
+package com.hellowordgae.mdh;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class HelloWorldGAEServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, Dynamic Confusion! And here's a demotivational quote:\nHe who chases rainbows may eventually catch tornadoes.");
+	}
+}
