679ad90f1edb5aef67457e9a82d5da03aadf8bec
Erica Terry <emae92@live.com>
2013-07-16 16:20:59 -0400

    Erica made an awesome change!

--- HelloWorldGAE/src/com/hellowordgae/mdh/HelloWorldGAEServlet.java
+++ HelloWorldGAE/src/com/hellowordgae/mdh/HelloWorldGAEServlet.java
@@ -9,5 +9,6 @@ public class HelloWorldGAEServlet extends HttpServlet {
 			throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, Dynamic Confusion! And here's a demotivational quote:\nHe who chases rainbows may eventually catch tornadoes!!!");
+		resp.getWriter().println("Hello, Dynamic Confusion! And here's a demotivational quote:\nLife is like a box of chocolates. It doesn't last as long for fat people.");
 	}
 }
